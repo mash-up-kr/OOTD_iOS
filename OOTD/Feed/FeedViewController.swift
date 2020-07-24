19a751df38987f03a04c6115ee71b04e71758c9c
jiyeong.private <popsmile7@gmail.com>
2020-07-24 19:06:34 +0900

    ✨피드 뷰 컨트롤러와 스토리보드 파일 생성

---
+++ OOTD/Feed/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  OOTD
+//
+//  Created by pony.tail on 2020/07/24.
+//  Copyright © 2020 fcs. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, StoryboardBuildable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
