69cd08cb45db82a29e5271ac1d56de2b805bc212
onemoon <sopthfg@gmail.com>
2020-07-25 17:48:21 +0900

    ✨ 메인 화면 베이스 작업 및 머지

---
+++ OOTD/Main/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  OOTD
+//
+//  Created by HyeonTae Kim on 2020/07/25.
+//  Copyright © 2020 fcs. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var headerWrapper: UIView!
+     
+}
