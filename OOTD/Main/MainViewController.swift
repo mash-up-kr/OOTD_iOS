a2b6484314feeaaf628fcbce925c0dd6d60587d8
onemoon <sopthfg@gmail.com>
2020-07-25 18:40:29 +0900

    🚀 IBOutlet

--- OOTD/Main/MainViewController.swift
+++ OOTD/Main/MainViewController.swift
@@ -10,5 +10,17 @@ import UIKit
 
 class MainViewController: UIViewController {
     @IBOutlet weak var headerWrapper: UIView!
-     
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var nowDateLabel: UILabel!
+    @IBOutlet weak var nowTemperatureLabel: UILabel!
+    @IBOutlet weak var lowestTemperatureLabel: UILabel!
+    @IBOutlet weak var highestTemperatureLabel: UILabel!
+    @IBOutlet weak var averageTemperatureLabel: UILabel!
+    @IBOutlet weak var nowWeatherImageView: UIImageView!
+    @IBOutlet weak var nowWeatherLabel: UILabel!
+    @IBOutlet weak var nowWeatherAdditionalInfoImageView: UIImageView!
+    @IBOutlet weak var nowWeatherAdditionalInfoLabel: UILabel!
+    @IBOutlet weak var updatedDateLabel: UILabel!
+    @IBOutlet weak var headerAddButton: UIButton!
+    
 }
