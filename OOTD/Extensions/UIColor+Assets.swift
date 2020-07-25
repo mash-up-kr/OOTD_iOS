3462e80d38bccf2edb500d30ed1235a46e30c45b
jiyeong.private <popsmile7@gmail.com>
2020-07-25 17:12:16 +0900

    🍱 정의된 에셋 컬러 UIColor 익스텐션에 추가

---
+++ OOTD/Extensions/UIColor+Assets.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Assets.swift
+//  OOTD
+//
+//  Created by pony.tail on 2020/07/25.
+//  Copyright © 2020 fcs. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blue = UIColor(named: "blue")!
+    static let blueKey = UIColor(named: "blueKey")!
+    static let grey01 = UIColor(named: "grey01")!
+    static let grey02 = UIColor(named: "grey02")!
+    static let grey03 = UIColor(named: "grey03")!
+    static let grey04 = UIColor(named: "grey04")!
+    static let grey05 = UIColor(named: "grey05")!
+    static let grey06 = UIColor(named: "grey06")!
+    static let grey07 = UIColor(named: "grey07")!
+    static let grey08 = UIColor(named: "grey08")!
+    static let grey09 = UIColor(named: "grey09")!
+    static let grey10 = UIColor(named: "grey10")!
+    static let red = UIColor(named: "red")!
+}
