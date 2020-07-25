965bbbe2c1c1e9ff5ed6bffe7a4a5b05d3c31b69
jiyeong.private <popsmile7@gmail.com>
2020-07-25 17:26:37 +0900

    💄뷰의 cornerRadius를 인터페이스빌더에서 수정가능하도록 추가

---
+++ OOTD/Extensions/UIView+Inspectable.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Inspectable.swift
+//  OOTD
+//
+//  Created by pony.tail on 2020/07/25.
+//  Copyright © 2020 fcs. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable public var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set { layer.cornerRadius = newValue }
+    }
+}
